ad311cba4753c7cd7b2cdbacdb2e238797989735
Saeed-Senior <nohchosaid@mail.ru>
2023-05-18 15:00:41 +0300

    first

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,48 +1,20 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-interface Detection {
-   order: string[];
-   caches: string[];
-}
+import translationEN from './locales/en/translation.json';
+import translationRU from './locales/ru/translation.json';
 
-interface Interpolation {
-   escapeValue: boolean;
-}
-
-interface BackendLang {
-      loadPath: string;
-}
-
-interface I18nConfig {
-   fallbackLng: string;
-   debug: boolean;
-   detection: Detection;
-   interpolation: Interpolation;
-   backend: BackendLang;
-
-}
-
-const i18nConfig: I18nConfig = {
-   fallbackLng: 'ru',
-   debug: true,
-   detection: {
-      order: ['queryString', 'cookie'],
-      caches: ['cookie']
+i18n.use(initReactI18next).init({
+   lng: 'en',
+   fallbackLng: 'en',
+   resources: {
+      en: {
+      translation: translationEN,
+      },
+      ru: {
+      translation: translationRU,
+      },
    },
-   interpolation: {
-      escapeValue: false
-   },
-   backend: {
-      loadPath: '/sayid/{{lng}}/{{ns}}.json',
-   },
-};
-
-i18n.use(Backend)
-   .use(LanguageDetector)
-   .use(initReactI18next)
-   .init(i18nConfig);
+});
 
 export default i18n;
